1658b0b64a50eb5221b5308537aee72704b16862
Jens Geudens <jensgeudens@hotmail.com>
2023-05-26 14:34:38 +0200

    Change to Install-ChocolateyInstallPackage (review comment)

--- modbusscope/tools/chocolateyinstall.ps1
+++ modbusscope/tools/chocolateyinstall.ps1
@@ -5,15 +5,12 @@ $fileLocation = Join-Path $toolsDir 'ModbusScope_setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
   file          = $fileLocation
-
-  softwareName  = 'ModbusScope'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
